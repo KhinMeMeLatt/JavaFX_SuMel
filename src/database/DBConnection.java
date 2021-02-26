a1037233ed57e8fceb67ec052e1679d0504f601e
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-02-26 23:44:40 +0630

    create Database connection

---
+++ src/database/DBConnection.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	private static String url = "jdbc:mysql://localhost:3306/sumeldb?useSSL=false";
+	private static String user = "root";
+	private static String password = "root";
+	
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
