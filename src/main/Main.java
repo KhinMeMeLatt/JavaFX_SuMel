93ecc3c8d06909386464ac35397feca0683d4dea
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-02-26 23:56:27 +0630

    Define frame width and height

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../view/LoginUI.fxml"));
-			Scene scene = new Scene(root);
+			Scene scene = new Scene(root,700,600);
 			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
