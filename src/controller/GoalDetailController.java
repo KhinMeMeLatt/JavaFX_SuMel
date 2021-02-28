67231d1da5920ad3600bdb02f3e00045bf7463ca
GICM-PC085 <GICM-PC085@GICM-PC085>
2021-03-01 00:28:02 +0630

    create goal detail

---
+++ src/controller/GoalDetailController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class GoalDetailController {
+
+    @FXML
+    private TableColumn<?, ?> lblGoal;
+
+    @FXML
+    private TableColumn<?, ?> lblGoalName;
+
+    @FXML
+    private TableColumn<?, ?> lblSave;
+
+    @FXML
+    private TableColumn<?, ?> lblWithdraw;
+
+    @FXML
+    private TableColumn<?, ?> lblBreak;
+
+    @FXML
+    private TableColumn<?, ?> lblHistory;
+
+    @FXML
+    void processJustSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void processSaveGoal(ActionEvent event) {
+
+    }
+
+}
