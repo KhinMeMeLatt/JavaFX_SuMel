4c2ea5d1111d329396aaa720aa6c41b90fdebbfd
thiri167 <57387350+thiri167@users.noreply.github.com>
2021-02-28 11:02:08 +0630

    Add files via upload

---
+++ src/view/SuMel/src/controller/User.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class User {
+	private static String email;
+	public static String getEmail() {
+		return email;
+	}
+	public static void setEmail(String email) {
+		User.email = email;
+	}
+
+}
