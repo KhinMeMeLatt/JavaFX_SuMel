eb5525eea7c0c4bc6709f6122c25cd3a35d5a7ce
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-03-04 17:57:25 +0630

    Fix Bugs

--- src/database/GoalDBModel.java
+++ src/database/GoalDBModel.java
@@ -5,13 +5,11 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import alert.AlertMaker;
 import model.accountModel.User;
 import model.subuModel.Goal;
 
