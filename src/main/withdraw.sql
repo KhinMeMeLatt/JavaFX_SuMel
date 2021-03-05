561b3c5ed2de540f4627f314eddb6a1b80d0771f
GICM-PC085 <GICM-PC085@GICM-PC085>
2021-03-05 23:36:05 +0630

    save and withdraw

---
+++ src/main/withdraw.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM sumeldb.withdraw
+LIMIT 0, 1000
+
+-- Date: 2021-03-05 23:34
+*/
+INSERT INTO `` (`withdrawId`,`withdrawAmount`,`withdrawAt`,`goalId`) VALUES (1,345,'2021-03-05 03:57:55',1);
+INSERT INTO `` (`withdrawId`,`withdrawAmount`,`withdrawAt`,`goalId`) VALUES (2,2000,'2021-03-05 23:10:45',1);
