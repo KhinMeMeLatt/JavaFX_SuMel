f599ea5021d1858de589eb8ee28cfe0abe4e266e
ThawMyoHtet <thawmyohtet001@gmail.com>
2021-03-09 06:16:50 +0630

    Countries added

--- src/controller/CurrencyController.java
+++ src/controller/CurrencyController.java
@@ -43,7 +43,26 @@ public class CurrencyController implements Initializable{
 	private JFXTextField txtAmount;
 
 	private String[] countriesList = {"United Arab Emirates (AED)","Afghanistan (AFN)"
-			,"Albania (ALL)","Armenia (AMD)","Netherlands Antilles (ANG)"};
+			,"Albania (ALL)","Armenia (AMD)","Netherlands Antilles (ANG)"
+			,"Angola (AOA)","Argentina (ARS)","Australia (AUD)","Aruba (AWG)","Azerbaijan (AZN)","Bosnia and Herzegovina (BAM)"
+			,"Barbados (BBD)","Bangladesh (BDT)","Bulgaria (BGN)","Bahrain (BHD)","Burundi (BIF)","Bermuda (BMD)","Brunei (BND)"
+			,"Bolivia (BOB)","Brazil (BRL)","Bahamas (BSD)","Bhutan (BTN)","Botswana (BWP)","Belarus (BYN)","Belize (BZD)"
+			,"Canada (CAD)","Democratic Republic of the Congo (CDF)","Switzerland (CHF)","Chile (CLP)","China (CNY)","Colombia (COP)"
+			,"Costa Rica (CRC)","Cuba (CUC)","Cuba (CUP)","Cape Verde (CVE)","Czech Republic (CZK)","Djibouti (DJF)","Denmark (DKK)"
+			,"Dominican Republic (DOP)","Algeria (DZD)","Egypt (EGP)","Eritrea (ERN)","Ethiopia (ETB)","European Union (EUR)","Fiji (FJD)"
+			,"Falkland Islands (FKP)","Faroe Islands (FOK)","United Kingdom (GBP)","Georgia (GEL)","Guernsey (GGP)","Ghana (GHS)","Gibraltar (GIP)"
+			,"The Gambia (GMD)","Guinea (GNF)","Guatemala (GTQ)","Guyana (GYD)","Hong Kong (HKD)","Honduras (HNL)","Croatia (HRK)","Haiti (HTG)"
+			,"Hungary (HUF)","Indonesia (IDR)","Israel (ILS)","Isle of Man (IMP)","India (INR)","Iraq (IQD)","Iran (IRR)","Iceland (ISK)","Jamaica (JMD)"
+			,"Jordan (JOD)","Japan (JPY)","Kenya (KES)","Kyrgyzstan (KGS)","Cambodia (KHR)","Kiribati (KID)","Comoros (KMF)","South Korea (KRW)","Kuwait (KWD)"
+			,"Cayman Islands (KYD)","Kazakhstan (KZT)","Laos (LAK)","Lebanon (LBP)","Sri Lanka (LKR)","Liberia (LRD)","Lesotho (LSL)","Libya (LYD)","Morocco (MAD)"
+			,"Moldova (MDL)","Madagascar (MGA)","North Macedonia (MKD)","Myanmar (MMK)","Mongolia (MNT)","Macau (MOP)","Mauritania (MRU)","Mauritius (MUR)","Maldives (MVR)"
+			,"Malawi (MWK)","Mexico (MXN)","Malaysia (MYR)","Mozambique (MZN)","Namibia (NAD)","Nigeria (NGN)","Nicaragua (NIO)","Norway (NOK)","Nepal (NPR)","New Zealand (NZD)"
+			,"Oman (OMR)","Panama (PAB)","Peru (PEN)","Papua New Guinea (PGK)","Philippines (PHP)","Pakistan (PKR)","Poland (PLN)","Paraguay (PYG)","Qatar (QAR)","Romania (RON)"
+			,"Serbia (RSD)","Russia (RUB)","Rwanda (RWF)","Saudi Arabia (SAR)","Solomon Islands (SBD)","Seychelles (SCR)","Sudan (SDG)","Sweden (SEK)","Singapore (SGD)","Saint Helena (SHP)"
+			,"Sierra Leone (SLL)","Somalia (SOS)","Suriname (SRD)","South Sudan (SSP)","Sao Tome and Principe (STN)","Syria (SYP)","Eswatini (SZL)","Thailand (THB)","Tajikistan (TJS)"
+			,"Turkmenistan (TMT)","Tunisia (TND)","Tonga (TOP)","Turkey (TRY)","Trinidad and Tobago (TTD)","Tuvalu (TVD)","Taiwan (TWD)","Tanzania (TZS)","Ukraine (UAH)","Uganda (UGX)"
+			,"United States (USD)","Uruguay (URU)","Uzbekistan (UZS)","Venezuela (VES)","Vietnam (VND)","Vanuatu (VUV)","Samoa (WST)","CEMAC (XAF)","Organisation of Eastern Caribbean States (XCD)"
+			,"International Monetary Fund (XDR)","CFA (XOF)","Collectives d'Outre-Mer (XPF)","Yemen (YER)","South Africa (ZAR)","Zambia (ZMW)"};
 	
 	private float getExchangeRate(String baseCountryCode, String destinationCountryCode) {
 		try {
